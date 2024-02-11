b6150b465682180a3f0c25e2198181de1b36c3d6
niera26 <niera26@outlook.com>
2024-02-11 16:17:59 +0100

    add api for whitelists proofs

---
+++ app/api/whitelists/[block_number]/[min_amount]/proofs/[address]/route.ts
@@ -0,0 +1,32 @@
+import prisma from "@/db"
+import { isAddress } from "viem"
+
+type Params = {
+    block_number: string
+    min_amount: string
+    address: string
+}
+
+export async function GET(request: Request, { params: { block_number, min_amount, address } }: { params: Params }) {
+    const blockNumber = BigInt(block_number)
+    const minAmount = BigInt(min_amount)
+
+    if (!isAddress(address)) {
+        return Response.error()
+    }
+
+    const result = await prisma.whitelists_proofs.findFirst({
+        select: {
+            proof: true,
+        },
+        where: {
+            block_number: { equals: blockNumber },
+            min_amount: { equals: minAmount.toString() },
+            address: { equals: address },
+        },
+    })
+
+    return Response.json({
+        proof: (result?.proof ?? []) as `0x${string}`[],
+    })
+}
