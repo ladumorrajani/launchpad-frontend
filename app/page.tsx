cb64d3a721daea26f97f4cb401baa7475b0b091f
niera26 <niera26@outlook.com>
2024-02-16 20:14:05 +0100

    put a not found page for home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import { redirect } from "next/navigation"
+import { notFound } from "next/navigation"
 
 export default function Home() {
-    redirect("/projects/14608/0x1bb3401DC800EED1Aed414cb335f3690186B0817")
+    // redirect("/projects/1/{contract_address}")
+    return notFound()
 }
