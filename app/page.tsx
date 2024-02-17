ff479e880a9ead33ea836a1105a35d21dccd31a3
niera26 <niera26@outlook.com>
2024-02-17 12:18:46 +0100

    add redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation"
 
 export default function Home() {
-    // redirect("/projects/1/{contract_address}")
-    return null
+    redirect("/projects/1/0x116Bb71512f8E9f76Df2840C7CED2B5Ee06f9C85")
 }
