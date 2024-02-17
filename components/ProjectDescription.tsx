ec7f29ed2cd597a1af997bff279f06a1038af1e2
niera26 <niera26@outlook.com>
2024-02-17 10:35:30 +0100

    remove the list from now

--- components/ProjectDescription.tsx
+++ components/ProjectDescription.tsx
@@ -3,12 +3,7 @@ import Link from "next/link"
 export function ProjectDescription() {
     return (
         <div>
-            <span>TaoBank is a decentralized borrowing protocol that allows you to draw interest-free loans against $wTAO used as collateral. Loans are facilitated in $taoUSD (a low-volatility stablecoin soft-pegged to the US dollar) and need to maintain an overcollateralized ratio. TaoBank as a protocol is non-custodial, immutable, and governance-free. Learn more in our <Link href="https://docs.taobank.ai/" target="_blank">documentation</Link>.</span>
-            <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
-                <li><Link href="/projects/1/0x0" target="_blank">Seed round (1000+ TPAD)</Link></li>
-                <li><Link href="/projects/1/0x0" target="_blank">TPAD round (50+ TPAD)</Link></li>
-                <li><Link href="/projects/1/0x0" target="_blank">Open round</Link></li>
-            </ul>
+            TaoBank is a decentralized borrowing protocol that allows you to draw interest-free loans against $wTAO used as collateral. Loans are facilitated in $taoUSD (a low-volatility stablecoin soft-pegged to the US dollar) and need to maintain an overcollateralized ratio. TaoBank as a protocol is non-custodial, immutable, and governance-free. Learn more in our <Link href="https://docs.taobank.ai/" target="_blank">documentation</Link>.
         </div>
     )
 }
