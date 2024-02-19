d81989c2f194c83ea87f4fc9ec6b73238431c3e5
niera26 <niera26@outlook.com>
2024-02-19 21:20:13 +0100

    redirect home to tpad round

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation"
 
 export default function Home() {
-    redirect("/projects/1/0x116Bb71512f8E9f76Df2840C7CED2B5Ee06f9C85")
+    redirect("/projects/1/0x481dacbf63363d142d761C6DE51da05F10A2b70D")
 }
