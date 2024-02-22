3377142270b173a65a07ec845f320a6add19b95c
niera26 <niera26@outlook.com>
2024-02-22 21:54:04 +0100

    use a metadata repository for cover image and description of the projects

--- components/ProjectCoverImage.tsx
+++ components/ProjectCoverImage.tsx
@@ -1,8 +1,36 @@
+"use client"
+
 import Image from "next/image"
+import { useState, useEffect } from "react"
+import { useContract } from "@/hooks/useContract"
 
 export function ProjectCoverImage() {
-    return <Image src="/taobank-cover.png" alt="taobank cover" width="1962" height="482" style={{
-        width: '100%',
-        height: 'auto',
-    }} />
+    const { chainId, address } = useContract()
+
+    const [exists, setExists] = useState<boolean>(false)
+
+    const url = `https://raw.githubusercontent.com/taopad/launchpad-metadata/master/${chainId}/${address}/cover.png`
+
+    useEffect(() => {
+        const fetchImageExists = async () => {
+            const response = await fetch(url, { method: "HEAD" })
+
+            if (response.status === 200) {
+                setExists(true)
+            }
+        }
+
+        fetchImageExists()
+    }, [url])
+
+    if (!exists) {
+        return null
+    }
+
+    return (
+        <Image src={url} alt="taobank cover" width="1962" height="482" style={{
+            width: '100%',
+            height: 'auto',
+        }} />
+    )
 }
--- components/ProjectDescription.tsx
+++ components/ProjectDescription.tsx
@@ -1,23 +1,31 @@
-import Link from "next/link"
+"use client"
+
+import Markdown from "react-markdown"
+import { useState, useEffect } from "react"
+import { useContract } from "@/hooks/useContract"
 
 export function ProjectDescription() {
-    return (
-        <div>
-            <p>
-                TaoBank is a decentralized borrowing protocol that allows you to draw interest-free loans against $wTAO used as collateral. Loans are facilitated in $taoUSD (a low-volatility stablecoin soft-pegged to the US dollar) and need to maintain an overcollateralized ratio. TaoBank as a protocol is non-custodial, immutable, and governance-free. Learn more in our <Link href="https://docs.taobank.ai/" target="_blank">documentation</Link>.
-            </p>
-            <ul className="mt-4 ml-6 list-disc [&>li]:mt-2">
-                <li>
-                    <Link href="/projects/1/0x116Bb71512f8E9f76Df2840C7CED2B5Ee06f9C85" target="_blank">
-                        TBANK Seed round (1000+ TPAD)
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/projects/1/0x481dacbf63363d142d761C6DE51da05F10A2b70D" target="_blank">
-                        TBANK Tpad round (50+ TPAD)
-                    </Link>
-                </li>
-            </ul>
-        </div>
-    )
+    const { chainId, address } = useContract()
+
+    const [markdown, setMarkdown] = useState<string>()
+
+    const url = `https://raw.githubusercontent.com/taopad/launchpad-metadata/master/${chainId}/${address}/description.md`
+
+    useEffect(() => {
+        const fetchDescription = async () => {
+            const response = await fetch(url)
+
+            if (response.status === 200) {
+                setMarkdown(await response.text())
+            }
+        }
+
+        fetchDescription()
+    }, [url])
+
+    if (markdown === undefined) {
+        return null
+    }
+
+    return <Markdown>{markdown}</Markdown>
 }
